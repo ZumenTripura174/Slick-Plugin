1a8d528382d32a87fe4b88b70ed8c33b9551ca7f
ZumenTripura174 <110328910+ZumenTripura174@users.noreply.github.com>
2022-11-16 12:05:05 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(function (){
+    $('.Parent').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrow:true,
+        prevArrow:".arrow .left",
+        nextArrow:".arrow .right",
+        dots: true,
+
+      });
+      
+      
+
+})
